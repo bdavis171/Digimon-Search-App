5636dccf0922797ccf156890e2d1bf28ebe76e4b
Brandon Davis <bdavis171@gmail.com>
2020-08-05 12:42:11 -0500

    deploying app

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ const port = process.env.PORT || 1997;
 const api = require('./routes/api');
 app.use('/api',api);
 
+app.use(express.static('../trex/build')); // render react when backend runs while deploying application
+
+// send react app
+app.use(function(req, res) {
+	res.sendFile(path.join(__dirname, '../trex/build/index.html'));
+});
+
 // CONNECTING TO A MONGO DATABASE
 // reference the mongoose module
 let mongoose = require('mongoose');
